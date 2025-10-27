4b6b22e909c74a69b88c0e4445c88c1584564643
mmarilao2025-pixel <mmarilao2025@alu.uct.cl>
2025-10-27 16:57:29 -0300

    Add: funciones lambda para varias operaciones

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+'''
+Crear una función lambda que reciba dos números y devuelva el mayor.
+lambda argumentos : expresion
+'''
+
+mayor = lambda x, y: x if x > y else y
+
+print(mayor(3, 5)) 
---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+'''
+Crear una función lambda que reciba un número y devuelva su doble.
+'''
+numero = lambda x: x*2
+print(numero(3))
